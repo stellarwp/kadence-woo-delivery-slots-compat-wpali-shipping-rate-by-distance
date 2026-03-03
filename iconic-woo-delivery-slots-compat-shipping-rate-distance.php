09944b3dd0fa47091fe44f5c92b540297af2bb91
Eric Defore <eric.defore@gmail.com>
2026-03-03 11:39:28 -0500

    chore: Update Author header from Kadence WP to Kadence

--- iconic-woo-delivery-slots-compat-shipping-rate-distance.php
+++ iconic-woo-delivery-slots-compat-shipping-rate-distance.php
@@ -3,7 +3,7 @@
  * Plugin Name:     WooCommerce Delivery Slots by Kadence [WpAli: Shipping Rate by Distance]
  * Plugin URI:      https://iconicwp.com/products/woocommerce-delivery-slots/
  * Description:     Compatibility between WooCommerce Delivery Slots by Kadence and Shipping Rate by distance by Ali Khallad
- * Author:          Kadence WP
+ * Author:          Kadence
  * Author URI:      https://www.kadencewp.com/
  * Text Domain:     iconic-woo-delivery-slots-compat-wpali-srbd
  * Domain Path:     /languages
